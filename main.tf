415f87d97eb66a4383276428a9254369112be79f
Sandip Karmakar <ablesudden3@gmail.com>
2020-10-01 02:43:11 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,45 @@
+provider "google" {
+  credentials = file("account.json")
+  project     = "home-test-226111"
+  region      = var.region
+
+}
+
+
+terraform {
+  backend "gcs" {
+    bucket = "asdk-123"
+    prefix = "demo"
+    credentials = "account.json"
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
