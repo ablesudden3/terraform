e002f5b14cfe5fe91c557cc97392261f364d7865
unknown <ablesudden3@gmail.com>
2020-12-23 13:09:29 +0530

    Megred subnet and VPC

--- main.tf
+++ main.tf
@@ -5,20 +5,16 @@ module "gcp_bucket" {
 
 }
 
-module "VPC_Network_Auto" {
 
-  source   = "github.com/ablesudden3/GCP_Networks"
-  VPC_Name = var.VPC_Name
-  region   = var.region
-  IP_Range = var.IP_Range
-}
 
-module "Subnets_for_VPC" {
+module "NW_Creation" {
 
   source   = "github.com/ablesudden3/GCP_Networks"
   region   = var.region
   VPC_Name = var.VPC_Name
   IP_Range = var.IP_Range
+
+  
 }
 
 
